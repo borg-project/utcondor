de25c861d02c5f37ed4b8192710927bb31ec319b
Bryan Silverthorn <bcs@cargo-cult.org>
2013-02-10 18:20:54 -0600

    Add setup.py; drop waf.

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    # actual setup
+    packages = setuptools.find_packages(),
+    install_requires = [
+        "plac>=0.9.1",
+        "pyzmq>=2.2.0.1",
+        ],
+    # description
+    name = "utcondor",
+    version = "0.4",
+    author = "Bryan Silverthorn",
+    author_email = "bcs@cargo-cult.org",
+    description = "tools for distributed computing at UTCS",
+    url = "https://github.com/bsilverthorn/utcondor",
+    keywords = "distributed condor UT UTexas cluster",
+    license = "MIT",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 2.6",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities"
+        ],
+    )
+
